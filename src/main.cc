1ef2e9c3e3edccea383bba893c988811dea40ad8
Alex Eden <alexandereden91@gmail.com>
2019-04-03 16:46:38 -0500

    Instantiate a display

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,15 @@
 #include <Streaming.h>
+#include "HyperDisplay_UG2856KLBAG01.h"
+
+UG2856KLBAG01_SPI oled;
 
 void setup() {
 	Serial.begin(115200);
 	Serial << "Ready" << endl;
+
+
+	SPI.begin(SCK, MISO, MOSI, SS);
+	oled.begin(A4, A5);
 }
 
 void loop() {
